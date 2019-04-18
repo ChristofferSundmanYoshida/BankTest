95945caf0d7bed27299717af291dd17597b518a8
ChristofferSundmanYoshida <christoffer.ekholm@gmail.com>
2019-04-18 14:53:05 +0200

    cucumber mm

--- features/konto_changes.feature
+++ features/konto_changes.feature
@@ -1,3 +1,9 @@
 Feature: As a customer I want to be able to create, change and delete accounts.
 
-Scenario:
+Scenario: Adding a new account named Test1
+Given my-accounts page loaded
+When I click the add account button
+And I get prompted to enter a name for the account
+And I enter Test1 as name
+And press Add account
+Then a new account named Test1 should have been created
--- step-definitions/konto_changes.js
+++ step-definitions/konto_changes.js
@@ -1,4 +1,27 @@
 module.exports = function() {
 
+    this.When(/^I click the add account button$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback(null, 'pending');
+      });
 
+      this.When(/^I get prompted to enter a name for the account$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback(null, 'pending');
+      });
+      
+      this.When(/^I enter Test(\d+) as name$/, function (arg1, callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback(null, 'pending');
+      });
+      
+      this.When(/^press Add account$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback(null, 'pending');
+      });
+
+      this.Then(/^a new account named Test(\d+) should have been created$/, function (arg1, callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback(null, 'pending');
+      });
 }
