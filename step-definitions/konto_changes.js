73c00f573bf6a3e347f1f3c481ac43ee0b7b53c4
ChristofferSundmanYoshida <christoffer.ekholm@gmail.com>
2019-04-23 09:19:44 +0200

    ändringar

--- step-definitions/konto_changes.js
+++ step-definitions/konto_changes.js
@@ -1,12 +1,45 @@
+let {$, sleep} = require('./funcs');
+
 module.exports = function() {
+           
 
-      this.When(/^I click the add account button$/, async function () {
-            await helpers.loadPage('http://localhost:3000/#my-accounts')
+      this.Given(/^my\-accounts page loaded$/, async function () {
+            // Write code here that turns the phrase above into concrete actions
+            await helpers.loadPage('http://localhost:3000/#login')
+            
+            let input = await driver.findElement(by.css('input#username'))
+            let password = await driver.findElement(by.css('input#password'))
+
+            let namn = 'Christoffer'
+            let pw = '123456'
+
+            input.sendKeys(namn)
+            password.sendKeys(pw)
+
+            await sleep(1000)
+            
+            await driver.findElement(by.css('button.btn.btn-primary.float-right.mt-3')).click()
+
+            await sleep(2000) 
 
 
-        
+
+
+
+
+            await helpers.loadPage('http://localhost:3000/#my-account')
 
       });
+
+      /*
+      this.When(/^I click the add account button$/, async function () {
+            // Write code here that turns the phrase above into concrete actions
+            let clicked = await driver.findElement(by.css('btn.btn-primary').click)
+
+            assert(clicked === true, 'fel')
+          
+      });
+      */        
 
       /*
       this.When(/^I get prompted to enter a name for the account$/, function () {
