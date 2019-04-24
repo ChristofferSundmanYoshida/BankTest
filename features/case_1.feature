09b39db78869b9f4f4769bdd56c736447a380c1b
Danadude <dan.olsen85@gmail.com>
2019-04-24 12:15:59 +0200

    dude

--- features/case_1.feature
+++ features/case_1.feature
@@ -1,11 +1,36 @@
-Feature: usc_2
+Feature: usc_1
 
    As a customer i want to be able to send/pay other people 
 
-
-Scenario: pewpew
+Scenario: BG transfer
 Given Överföringar andra konton has loaded
-And I have Entered the correct information
+And I have Entered the correct BG information
 When i click the send button
 And i Click the confirmation promt
 Then i should have the sum taken from my account
+
+Scenario: PG transfer
+Given Överföringar andra konton has loaded
+And I have Entered the correct PG information
+When i click the send button
+And i Click the confirmation promt
+Then i should have the sum taken from my account
+
+Scenario: Other transfer
+Given Överföringar andra konton has loaded
+And I have Entered the correct Other information
+When i click the send button
+And i Click the confirmation promt
+Then i should have the sum taken from my account
+
+Scenario: over the transfer limit
+Given Överföringar andra konton has loaded
+And I have Entered the correct information for transfer over the limit
+When i click the send button
+Then I should recieve an error message
+
+Scenario: over the account balance
+Given Överföringar andra konton has loaded
+And I have Entered the correct information for transfer over the account balance
+When i click the send button
+Then I should recieve an error message
--- step-definitions/case_1.js
+++ step-definitions/case_1.js
@@ -1,38 +1,103 @@
 let { $, sleep } = require("./funcs");
     let namn = "gsimnett0";
     let pw = "TbObbUKQ";
+    let balance
+    let bgkonto = "111-1111"
+    let pgkonto = "111111-1"
+    let akonto = "123456789-10"
+    let amount = "1"
+    let message = `vi skickar ${amount} till ${bgkonto}`
 
 module.exports = function() {
     this.Given(/^Överföringar andra konton has loaded$/,async function () {
         await helpers.loadPage("http://localhost:3000/#login");
-
+        //sends the login info.
         let input = await $("input#username");
         let password = await $("input#password");
-    
         input.sendKeys(namn);
         password.sendKeys(pw);
-    
+        //clicks the submit.
         let logon = await $(".login-form button");
         await logon.click();
-        let myAccount = await $(".nav li:nth-of-type(5)");
+        await sleep(500)
+        //getting balance to compare later.
+        let myAccount = await $(".nav li:nth-of-type(4)");
         await myAccount.click();
-
-//        let currUrl = driver.getCurrentUrl();
-  //      console.log(currUrl[1])
-       // assert(currUrl === "http://localhost:3000/#transfer", 'fel');
-
-
+        await sleep(500)
+        balance = await driver.findElement(by.css('tr:nth-child(1) > td.text-right')).getText()
+        //remove all characters that are not numbers, and converting to an int.
+        balance = balance.replace(/\D/g, '') / 100;
+        //Enters the transfer page
+        let myTransfer = await $(".nav li:nth-of-type(5)");
+        await myTransfer.click();
+        await sleep(500)
+        let siteCheck = await driver.findElement(by.css(".row article p")).getText()
+        assert(siteCheck === "Skriv in kontouppgifter för önskat konto att skicka pengar till.");
       });
 
-      this.Given(/^I have Entered the correct information$/, function () {
+      this.Given(/^I have Entered the correct BG information$/,async function () {
+        //Fill the send Form.
+        await driver.findElement(by.css("input[value='bg']")).click()
+        await driver.findElement(by.css("input#toAccountNumber.form-control")).sendKeys(bgkonto)
+        await driver.findElement(by.css("input#sum.form-control")).sendKeys(amount)
+        await driver.findElement(by.css("input#label.form-control")).sendKeys(message)
       });
       
-      this.When(/^i click the send button$/, function () {
+      this.When(/^i click the send button$/,async function () {
+        //clicks submit.
+        let submit = await $(".transfer-form button");
+        await submit.click();
       });     
 
-      this.When(/^i Click the confirmation promt$/, function () {
+      this.When(/^i Click the confirmation promt$/,async function () {
+        //clicks the alert prompt.
+        let alert = await driver.switchTo().alert()
+        await alert.accept()
       });
 
-      this.Then(/^i should have the sum taken from my account$/, function () {
+      this.Then(/^i should have the sum taken from my account$/,async function () {
+        balanceAfter = await driver.findElement(by.css('tr:nth-child(1) > td.text-right')).getText()
+        //remove all characters that are not numbers, and converting to an int.
+        balanceAfter = balanceAfter.replace(/\D/g, '') / 100;
+        //comparing expected value with real value.
+        assert(balance - amount === balanceAfter,"fel")
+      });
+
+      this.Given(/^I have Entered the correct PG information$/,async function () {
+        //Fill the send Form.
+        await driver.findElement(by.css("input[value='pg']")).click()
+        await driver.findElement(by.css("input#toAccountNumber.form-control")).sendKeys(pgkonto)
+        await driver.findElement(by.css("input#sum.form-control")).sendKeys(amount)
+        await driver.findElement(by.css("input#label.form-control")).sendKeys(message)
+      });
+
+      this.Given(/^I have Entered the correct Other information$/,async function () {
+        //Fill the send Form.
+        await driver.findElement(by.css("input[value='ta']")).click()
+        await driver.findElement(by.css("input#toAccountNumber.form-control")).sendKeys(akonto)
+        await driver.findElement(by.css("input#sum.form-control")).sendKeys(amount)
+        await driver.findElement(by.css("input#label.form-control")).sendKeys(message)
+      });
+
+      this.Given(/^I have Entered the correct information for transfer over the limit$/,async function () {
+        //Fill the send Form.
+        await driver.findElement(by.css("input[value='ta']")).click()
+        await driver.findElement(by.css("input#toAccountNumber.form-control")).sendKeys(akonto)
+        await driver.findElement(by.css("input#sum.form-control")).sendKeys("50000")
+        await driver.findElement(by.css("input#label.form-control")).sendKeys(message)
+      });
+
+      this.Then(/^I should recieve an error message$/,async function () {
+        let errorCheck = await driver.findElement(by.css("small.error")).getText()
+        assert(errorCheck === "Du kan inte överföra över 30000 under de senaste 7 dagarna." || "Du har inte tillräckligt med pengar.");
+        console.log(errorCheck)
+      });
+
+      this.Given(/^I have Entered the correct information for transfer over the account balance$/,async function () {
+        //Fill the send Form.
+        await driver.findElement(by.css("input[value='ta']")).click()
+        await driver.findElement(by.css("input#toAccountNumber.form-control")).sendKeys(akonto)
+        await driver.findElement(by.css("input#sum.form-control")).sendKeys("5000000")
+        await driver.findElement(by.css("input#label.form-control")).sendKeys(message)
       });
 };
