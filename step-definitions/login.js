057497aaca4a0ff2307dd8e457ec69c143813d86
eyeq1337 <eyeq24@gmail.com>
2019-04-25 09:45:05 +0200

    us 01 added

--- step-definitions/login.js
+++ step-definitions/login.js
@@ -1,12 +1,56 @@
-await helpers.loadPage('http://localhost:3000/#login')
-            
-            let input = await driver.findElement(by.css('input#username'))
+let { $, sleep } = require('./funcs');
+
+let namn = 'David'
+let pw = '123456'
+
+'body > main > div > aside > nav > ul > li:nth-child(3) > button'
+
+module.exports = function () {
+
+      this.Given(/^start page loaded$/, async function () {
+
+            await helpers.loadPage('http://localhost:3000/#start')
+
+            await sleep(100)
+
+            await sleep(100)
+
+      });
+
+      this.When(/^I click the add account button$/, async function () {
+
+            await driver.findElement(by.css('body > main > div > aside > nav > ul > li:nth-child(3) > button')).click()
+
+      });
+      this.When(/^I enter a new account name, a new password and reapeat my password$/, async function () {
+
+            let inputUsername = await driver.findElement(by.css('input#username'))
             let password = await driver.findElement(by.css('input#password'))
-            let namn = 'dav'
-            let pw = '123456'
-            input.sendKeys(namn)
-            password.sendKeys(pw)
-            await sleep(1000)
-            
-            await driver.findElement(by.css('button.btn.btn-primary.float-right.mt-3')).click()
-            await sleep(2000) 
+            let inputRepeated = await driver.findElement(by.css('input#passwordRepeated'))
+
+            await inputUsername.sendKeys(namn)
+            await password.sendKeys(pw)
+            await inputRepeated.sendKeys(pw)
+
+      });
+      this.When(/^press Add account$/, async function () {
+
+            await driver.findElement(by.css('body > main > div > article > form > button')).click()
+
+      });
+
+      this.Then(/^a new account named David should have been created$/, async function () {
+
+            await sleep(500)
+
+            konto = await driver.findElement(by.css('body header small span')).getText()
+
+            console.log(konto)
+
+            assert(konto === 'David', 'fel')
+
+            await sleep(100)
+
+      });
+
+}
